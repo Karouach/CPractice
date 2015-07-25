d315eedfedd6da6b821bca043dd93c1f09eb75a5
Ibtissam <ik@karouach.se>
2015-07-25 18:34:53 +0200

    Linked Lists - Delete Node at Nth Position

--- linkedlist2.c
+++ linkedlist2.c
@@ -5,48 +5,41 @@ struct Node {
 int data;
     struct Node *next;
 };
-struct Node *head;
-void Insert (int data, int n);
+struct Node*head;
+void Insert(int data);
 void Print();
+void Delete(int n);
 
-void Insert(int data, int n){
-    struct Node *temp1 = (struct Node*)malloc(sizeof(struct Node*));
-    temp1->data = data;
-    temp1->next = NULL;
-    if(n == 1){
-        temp1->next = head;
-        head = temp1;
+void Delete(int n){
+    struct Node *temp1 = head;
+    if(n ==1) {
+        head = temp1->next; //head now points to second node
+        free(temp1);
         return;
-    }
-    struct Node *temp2 = head;
-    int i;
-    for (i = 0; i < n -2; i++){
-        temp2 = temp2->next;
-    }
-    temp1->next = temp2->next;
-    temp2 ->next= temp1;
-}
-void Print(){
-    struct Node *temp = head;
-    while (temp!=NULL){
-        printf("%d", temp->data);
-        temp = temp->next;
 
     }
-    printf("\n");
+    int i;
+    for(i = 0; i < n-2; i++)
+        temp1 = temp1->next; //temp points out to (n-1) th node
+    struct Node *temp2 = temp1->next; //nth node
+    temp1->next = temp2->next;
+    free(temp2);
 }
 
 
 
 int main(){
-    head = NULL; //lest empty initially
-    Insert(2, 1);
-    Insert(3, 2);
-    Insert(4, 1);
-    Insert(5, 2);
+    head = NULL;
+    Insert(2);
+    Insert(4);
+    Insert(6);
+    Insert(5);
     Print();
-
-
+    int n;
+    printf("Enter a number\n");
+    scanf("%d", &n);
+    Delete(n);
+    Print();
 
 
 }
