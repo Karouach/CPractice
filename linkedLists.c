4f9662413d05398bf21ee9ed3fbdb6127248a129
Ibtissam <ik@karouach.se>
2015-07-26 10:34:34 +0200

    Onw Way Linked Lists - Add a Node at Beginning

--- linkedLists.c
+++ linkedLists.c
@@ -9,24 +9,32 @@
 
 struct node {
     int info;
-    struct mylist *link;
+    struct node *link;
 };
 
 void display(struct node *start);
+struct node *addatbeg(struct node *start, int num);
 
 
 int main()
 {
     struct node *start = NULL;
-    int choice;
+    int choice, num;
     while (1){
-        printf("1. Display\n\n");
+        printf("n1. Display\n\n");
+        printf("2. Add Node at The begining\n\n");
         printf("9. Exit\n\n");
         scanf("%d", &choice);
         switch (choice){
             case 1:
                 display(start);
                 break;
+            case 2:
+                printf("Add the number to be inserted\n\n");
+                scanf("%3d", &num);
+                start = addatbeg(start, num);
+                break;
+
             case 9:
                 exit(1);
 
@@ -51,5 +59,14 @@ void display(struct node *start){
             printf("%d", p->info);
             p = p->link;
         }
+
+}
+struct node *addatbeg(struct node *start, int num){
+    struct node *temp;
+    temp = (struct node *) malloc(sizeof(struct node));
+    temp ->info = num;
+    temp->link = start;
+    start = temp;
+    return start;
 
 }
